ca6ded7f591f4a0fc3c430bbb126b3a4be413735
patricksudol <patrick.sudol@outlook.com>
2020-02-09 00:40:31 -0500

    Refactor

--- referee/models/referee.py
+++ referee/models/referee.py
@@ -3,6 +3,7 @@ from django.db import models
 from base.models.abstract_base_model import BaseModel
 
 
+
 class Referee(BaseModel):
 
     referee_id = models.CharField(max_length=7)
@@ -23,6 +24,34 @@ class Referee(BaseModel):
 
     is_active_onfield = models.BooleanField(default=False)
 
+    image_path = models.CharField(max_length=255, null=True, blank=True)
+
+    @property
+    def ar_assignments(self):
+        return self.ar1_matches.all() | self.ar2_matches.all()
+
+    @property
+    def all_assignments(self):
+        return self.referee_matches.all() | self.ar1_matches.all() | self.fourth_official_matches.all()
+
+    @property
+    def yellow_card_count(self):
+        return self.yellowcard_set.count()
+
+    @property
+    def red_card_count(self):
+        return self.redcard_set.count()
+
+    # Hacky AF but placeholder into I can collect all images and host locally
+    @property
+    def image_url(self):
+        url = (
+            f'http://proreferees.com/wp-content/uploads/2020/02/2020-'
+            f'{self.name_given}-{self.name_family}.jpg'
+        )
+        return self.image_path if self.image_path else url
+
     def __str__(self):
         status = 'active' if self.is_active else 'not active'
         return f'{self.name_common} [{self.referee_id}] is {status}'
+
--- referee/views.py
+++ referee/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 
+from referee.models.referee import Referee
+
 def referees(request):
     context = {}
+    refs = Referee.objects.all().order_by('name_family')[:10]
+    counter = 1
+    referees_list = []
+    for ref in refs:
+        all = ref.all_assignments
+        debut = all.order_by('match_date').first() if all else ''
+        ref_dict = {
+            'name': ref.name_common,
+            'total_assignments_count': all.count(),
+            'image_url': ref.image_url,
+            'debut_match': {
+                'date': debut.match_date if debut else 'TBD',
+                'home_team': debut.home_club.name if debut else '',
+                'away_team': debut.away_club.name if debut else ''
+            },
+            'count': counter,
+            'first_in_row': True if counter % 4 == 1 else False
+        }
+        referees_list.append(ref_dict)
+        counter += 1
+    context['referees'] = referees_list
+    context['nbar'] = 'referees'
     return render(request, 'referees.html', context)
 
