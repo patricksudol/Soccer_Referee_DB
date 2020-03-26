40472695c206d09f0dc72b39124a35aa1ac2bf57
Patrick Sudol <patricksudol@patricks-mbp.lan>
2020-03-26 00:03:55 -0400

    Start club section

--- organization/urls.py
+++ organization/urls.py
@@ -1,8 +1,8 @@
 from django. urls import *
 
-from organization.views import *
+from organization.views import clubs, club_bio
 
 urlpatterns = [
     path('clubs', clubs, name="clubs"),
-    path('referees/<str:club_id>/', club_bio, name="club_bio")
+    path('clubs/<str:club_id>/', club_bio, name="club_bio")
 ]
--- organization/views.py
+++ organization/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from organization.models import Club
+
 def clubs(request):
-    pass
+    clubs = Club.objects.all()
+    context = {'nbar': 'clubs', 'clubs': clubs}
+    return render(request, 'clubs.html', context)
 
 def club_bio(request, club_id):
     pass
